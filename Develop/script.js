99895e06a3e0335b9126f8c5af863ece5462c48d
Hunter Padgett <gizmokabanjo@gmail.com>
2022-06-25 00:27:46 -0400

    added if statements for prompts & for loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,76 +1,70 @@
-// Assignment code here
+
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+// all possible characters for random password
+var specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "[", "}", "{", "`", "~", ".", ","];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbersPw = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var charOptions = []
+
+
 
 //validate the input
 //generate password based on citeria 
-//display/return password to the page
 
 
-
-// all possible characters for random password
-
-var specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "[", "}", "{"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var charLenth = 8
-var min = 8
-var max = 128
-
-//store user prompts in a variable
-var passLength = [];
-
-
-//promt user for password criteria
-
-  //prompt password length between 8 - 128
-function generateLength() {
-
-  var min = 8
-  var max = 128
-
-  var lengthPrompt = prompt("How long would you like your password to be?", "Please enter a number between 8 - 128!"); 
-    if (lengthPrompt >= 8 && lengthPrompt <= 128) {
-      passLength = lengthPrompt;
-      return;
-
-    } else {
-      alert("Invalid entry. Please enter a number between " + min + " and " + max + ".")
-      return generateLength();
+function generatePassword() {
+  
+    //promt user for password criteria 
+    //pass length between 8 - 128
+  var lengthPrompt = prompt("Please enter a number between 8 - 128 to generate your password."); 
+    if (!(lengthPrompt >= 8 && lengthPrompt <= 128)) {
+      alert("Invalid entry. Please enter a number between 8 and 128.");
+      generatePassword();
     }
+
+
+    //confirm lowercase, uppercase, numbers, spec char 
+  var lowers = confirm("Click OK if you would like lowercase letters in your password?");
+
+  if (lowers) {
+    charOptions = charOptions.concat(lowerCase);
+    
   }
 
-function generatePrompts() {    
-    //confirm lowercase, uppercase, numbers, spec char 
-  var lowerCase = confirm("Would you like lowercase letters in your password?");
-    if (lowerCase) {
-      passLength += lowerCase;
-    };
-   
+  var uppers = confirm("Click OK if you would like uppercase letters in your password?");
 
-  var upperCase = confirm("Would you like uppercase letters in your password?");
-    if (upperCase) {
-      passLength += upperCase;
-    };
-    
+  if (uppers) {
+    charOptions = charOptions.concat(upperCase);
 
-  var specChar = confirm("Would you like special characters in your password?");
-    if (specChar) {
-      passLength += specChar;
-    };
+  }
 
-  var numbers = confirm("Would you like numbers in your password?");
-    if (numbers) {
-      passLength += numbers;
-    };
+  var specs = confirm("Click OK if you would like special characters in your password?");
+
+  if (specs) {
+    charOptions = charOptions.concat(specChar);
+
+  }
+
+  var nums = confirm("Click OK if you would like numbers in your password?");
+
+  if (nums) {
+    charOptions = charOptions.concat(numbersPw);
+
+  }
+
+  var passLength = '';
+  for (var i = 0; i < lengthPrompt; i++) {
+    passLength += charOptions[Math.floor(Math.random() * charOptions.length)];
+  }  
+  //display/return password to the page
+  return passLength
 }
 
-generateLength();
-generatePrompts();
-generatePassword();
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
